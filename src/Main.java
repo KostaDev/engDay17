41a55fe5fd70535c4d5ff90042884509c5fcdc3a
Kostadin Sajic <kokiftw93@gmail.com>
2022-02-25 09:44:32 +0100

    added 2 watches in watch branch

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,7 @@ import domain.MehanicalWatch;
 public class Main {
     public static void main(String[] args) {
         Entity watch = new MehanicalWatch(12314);
+        Entity watch2 = new MehanicalWatch(12314);
+
     }
 }
