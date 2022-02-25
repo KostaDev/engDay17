7dcdc580520107990303e8c18ee9410e5719036c
Kostadin Sajic <kokiftw93@gmail.com>
2022-02-25 10:09:45 +0100

    new mechanical wathc added 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import domain.Entity;
+import domain.MehanicalWatch;
+
 public class Main {
     public static void main(String[] args) {
-
+        Entity watch = new MehanicalWatch(12314);
     }
 }
